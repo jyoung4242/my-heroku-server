628a917df704ca80e5320c26268de929a6e39731
Justin Young <justin_dean_young@yahoo.com>
2022-01-26 23:50:44 -0500

    added root path

--- src/index.js
+++ src/index.js
@@ -3,5 +3,6 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.listen(port, () => {
+  .get('/', (req, res) => res.render('pages/index'))
   console.log(`Example app listening on port ${port}`)
 })
