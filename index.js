68cbf42ac5e1c5f2cbf75fe412a5189f881395d3
Justin Young <justin_dean_young@yahoo.com>
2022-01-27 00:57:17 -0500

    added / route console log

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => {})
+app.get("/", (req, res) => {
+  console.log("empty page")
+})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
