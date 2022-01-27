d0bb3192ce094c04bbd32ca1e8010cf1f5235444
Justin Young <justin_dean_young@yahoo.com>
2022-01-26 23:53:31 -0500

    fixed route

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 5000
 
+app.get("/", (req, res) => res.render("pages/index"))
+
 app.listen(port, () => {
-  .get('/', (req, res) => res.render('pages/index'))
   console.log(`Example app listening on port ${port}`)
 })
