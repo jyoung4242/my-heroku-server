1511d21c96c52c2b17c34a7ca5b55ff5162cbbdd
Justin Young <justin_dean_young@yahoo.com>
2022-01-26 22:58:07 -0500

    updated port variable

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 const express = require("express")
 const app = express()
-const port = 5001
-
-app.get("/", (req, res) => {
-  res.send("Hello World!")
-})
+const PORT = process.env.PORT || 5000
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
-
-//console.log("end of file")
