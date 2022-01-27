87aa7f8cd2cd12567418117274e546efa52ac09f
Justin Young <justin_dean_young@yahoo.com>
2022-01-27 00:00:18 -0500

    fixed port

--- index.js
+++ index.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 5000
 app.get("/", (req, res) => res.render("pages/index"))
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on port ${PORT}`)
 })
