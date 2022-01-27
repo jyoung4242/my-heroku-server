b92c062e95569c9662e9d84ffc495f32bddc5477
Justin Young <justin_dean_young@yahoo.com>
2022-01-27 00:55:41 -0500

    removed res.render

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express")
 const app = express()
 const PORT = process.env.PORT || 5000
 
-app.get("/", (req, res) => res.render("pages/index"))
+app.get("/", (req, res) => {})
 
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}`)
