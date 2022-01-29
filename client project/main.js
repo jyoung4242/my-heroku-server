95cfe45451e8ee5a3284d2827247f2e5392e011f
Justin Young <justin_dean_young@yahoo.com>
2022-01-29 12:31:42 -0500

    client

---
+++ client project/main.js
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+//buttons
+const createButton = document.getElementById("createButton")
+const updateButton = document.getElementById("updateButton")
+const readButton = document.getElementById("readButton")
+const deleteButton = document.getElementById("deleteButton")
+
+//inputs
+const username = document.getElementById("username")
+const password = document.getElementById("password")
+const email = document.getElementById("email")
+const Uusername = document.getElementById("updateusername")
+const Npassword = document.getElementById("newpassword")
+const Nemail = document.getElementById("newemail")
+const delUsername = document.getElementById("removeusername")
+
+function init() {}
