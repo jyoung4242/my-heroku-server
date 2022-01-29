da1bb2cb3803611e0f519d3d83b83950f2cea967
Justin Young <justin_dean_young@yahoo.com>
2022-01-29 13:58:49 -0500

    updated response status

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express()
 const PORT = process.env.PORT || 5000
 
 app.get("/", (req, res) => {
-  res.send("Hello world")
+  res.status(200).send("Hello world")
 })
 
 app.listen(PORT, () => {
